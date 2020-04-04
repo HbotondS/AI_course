5984e0725a3ba5fc3760f166114e9571394b0672
Nyulas <nyulashunor98@gmail.com>
2020-04-04 18:43:21 +0300

    minimax function

--- minmax/Gomoku.py
+++ minmax/Gomoku.py
@@ -1,8 +1,37 @@
 # minmax AI
+import math
 
-def bestMove():
+#def bestMove():
     # todo
 
 
-def minmax():
-    # todo
+def minmax(board, depth, isMax):
+    result = CheckWinner()
+    if result == 1:
+        return "Player won!"
+    elif result == -1:
+        return "Computer won"
+    elif result == 0:
+        return "Tie"
+
+    if isMax:
+        bestScore = -math.inf
+        for i in range(16):
+            for j in range(16):
+                if board[i][j] == 0:
+                    board[i][j] = -2
+                    score = minmax(board, depth + 1, False)
+                    board[i][j] = 0
+                    bestScore = max(score, bestScore)
+
+        return bestScore
+    else:
+        bestScore = math.inf
+        for i in range(16):
+            for j in range(16):
+                if board[i][j] == 0:
+                    board[i][j] = 2
+                    score = minmax(board, depth + 1, True)
+                    board[i][j] = 0
+                    bestScore = min(score, bestScore)
+        return bestScore
