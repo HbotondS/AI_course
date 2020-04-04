3aa31ecf26603c4a4ef164bed2a9c8589bcf26ea
Nyulas <nyulashunor98@gmail.com>
2020-04-04 20:32:12 +0300

    winneCheck func

--- minmax/Gomoku.py
+++ minmax/Gomoku.py
@@ -6,6 +6,49 @@ ai = -2
 human = 2
 board = np.zeros((15, 15), dtype=int)
 
+
+def winnerCheck():
+    winner = 0
+
+    #Horizontal check
+    for i in range(15):
+        for j in range(15):
+            if board[i][j] == board[i][j+1] == board[i][j+2] == board[i][j+3] == board[i][j+4]:
+                winner = board[i][j]
+                return winner
+
+    #vertical check
+    for i in range(15):
+        for j in range(15):
+            if board[j][i] == board[j+1][i] == board[j+2][i] == board[j+3][i] == board[j+4][i]:
+                winner = board[j][i]
+                return winner
+
+    #diagonal check
+    for i in range(11):
+        checkdiagonal = board.diagonal(i)
+        for j in range(len(checkdiagonal)):
+            if checkdiagonal[j] == checkdiagonal[j+1] == checkdiagonal[j+2] == checkdiagonal[j+3] == checkdiagonal[j+4]:
+                winner = checkdiagonal[j]
+                return winner
+
+    for i in range(11):
+        checkdiagonal = board.diagonal(-i)
+        for j in range(len(checkdiagonal)):
+            if checkdiagonal[j] == checkdiagonal[j+1] == checkdiagonal[j+2] == checkdiagonal[j+3] == checkdiagonal[j+4]:
+                winner = checkdiagonal[j]
+                return winner
+
+    emptyPlace = 0
+    for i in range(15):
+        for j in range(15):
+            if board[i][j] == 0:
+                emptyPlace += 1
+
+    if emptyPlace == 0:
+        return 0
+
+
 def bestMove():
     # AI turn
     bestScore = -math.inf
@@ -24,9 +67,9 @@ def bestMove():
 
 def minmax(board, depth, isMax):
     result = CheckWinner()
-    if result == 1:
+    if result == 2:
         return "Player won!"
-    elif result == -1:
+    elif result == -2:
         return "Computer won"
     elif result == 0:
         return "Tie"
